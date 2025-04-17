0b04ad747cfed37940d8fd7271cbcb403b302eed
eurekasolution <kjmin@daum.net>
2025-04-17 16:22:46 +0900

    2025-04-17A

--- README.txt
+++ README.txt
@@ -28,3 +28,24 @@ HWP파일의 이름 : DC#01한문학과-홍길동.hwp
 KAKAO : 0afda05de8855f9286462ad89edf999d
 
 36.368491, 127.342039
+
+https://github.com/eurekasolution/cju-smart-home
+
+
+Q1. 다음과 같은 홈페이지를 만들고 싶어.
+HTML5와 Bootstrap5를 이용하고, PHP로 프로그래밍할거야.
+index.php파일을 만들건데,
+상단은 navbar를 이용해 드랍다운 메뉴를 만들거야.
+navbar에는 홈, 메뉴1, 메뉴2가 있어.
+메뉴1에는 메뉴1-1, 메뉴1-2로 구성되어있고
+메뉴2에는 메뉴2-1, 메뉴2-2로 구성되어 있어.
+
+몸체부분에는 index.php?cmd=값 형태에서
+cmd가 만약 test이면
+include "$cmd.php"를 수행하는데, 이때는 include "test.php"야.
+
+만약에 $cmd가 없으면 $cmd="init"로 설정해줘.
+바디 아래에는 사이트 정보를 출력하는데
+"충남대학교 인문대학 한문학과"라고 출력해 줘.
+그런데 Body부분의 내용이 너무 적을 떄는 사이트 정보를 화면의 
+맨 아래에 배치하도록 해 줘.
--- index.php
+++ index.php
@@ -1,5 +1,88 @@
-충남대학교 인문대학 한문학과<br>
-학번 : 202212345<br>
-이름 : 홍길동<br>
+<?php
+// cmd 파라미터 처리
+$cmd = isset($_GET['cmd']) ? $_GET['cmd'] : 'init';
+?>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <title>홈페이지</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Bootstrap 5 CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <style>
+    html, body {
+      height: 100%;
+    }
+    body {
+      display: flex;
+      flex-direction: column;
+    }
+    main {
+      flex: 1; /* 남은 공간을 모두 main이 차지하게 함 */
+    }
+  </style>
+</head>
+<body>
 
-수정하기<br>
+  <!-- 네비게이션 바 -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">홈페이지</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">홈</a>
+          </li>
+
+          <!-- 메뉴1 -->
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="menu1" role="button" data-bs-toggle="dropdown">
+              메뉴1
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="index.php?cmd=menu1-1">메뉴1-1</a></li>
+              <li><a class="dropdown-item" href="index.php?cmd=menu1-2">메뉴1-2</a></li>
+            </ul>
+          </li>
+
+          <!-- 메뉴2 -->
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="menu2" role="button" data-bs-toggle="dropdown">
+              메뉴2
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="index.php?cmd=menu2-1">메뉴2-1</a></li>
+              <li><a class="dropdown-item" href="index.php?cmd=menu2-2">메뉴2-2</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- 메인 콘텐츠 -->
+  <main class="container my-4">
+    <?php
+      $file = "$cmd.php";
+      if (file_exists($file)) {
+        include $file;
+      } else {
+        echo "<p>요청하신 페이지를 찾을 수 없습니다.</p>";
+      }
+    ?>
+  </main>
+
+  <!-- 푸터 -->
+  <footer class="bg-light text-center text-muted py-3">
+    충남대학교 인문대학 한문학과
+  </footer>
+
+  <!-- Bootstrap 5 JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
