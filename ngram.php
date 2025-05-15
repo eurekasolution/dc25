7b6e950b5e5f14fcacf23cac62f90b9b32c42831
eurekasolution <kjmin@daum.net>
2025-05-15 16:36:13 +0900

    2025-05-15A

---
+++ ngram.php
@@ -0,0 +1,8 @@
+<form>
+    <div class="row">
+        <div class="col-10 colLine">
+            <textarea class="form-control" name="text" rows="10"></textarea>
+        </div>
+        <div class="col colLine">버튼</div>
+    </div>
+</form>
